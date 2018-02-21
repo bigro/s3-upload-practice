64595cf9aedf19027e24f89fc779c7b518e33169
Yuuki Ooguro <kitkat.jaja@gmail.com>
2018-02-21 15:48:14 +0900

    ファイルをアップロードできるが、なぜかファイルの内容がfilePathになっている

---
+++ src/main/java/PutObject.java
@@ -0,0 +1,37 @@
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+public class PutObject {
+
+
+    public static void main(String[] args) throws Exception {
+        String bucket_name = "kit-sandbox";
+        String file_path = "/Users/ooguro/Documents/git/s3-upload-practice/out/production/resources/hello.txt";
+        String key_name = Paths.get(file_path).getFileName().toString();
+
+        System.out.format("Uploading %s to S3 bucket %s...\n", file_path, bucket_name);
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAJFJI6U253HMPHLAQ", "q4s/2zlwR9UdbscYl1V+AGPUk/v9lETemYI/danB");
+        AmazonS3 s3 = AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.AP_NORTHEAST_1)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .build();
+        try {
+            s3.putObject(bucket_name, key_name, file_path);
+        } catch (AmazonServiceException e) {
+            System.err.println(e.getErrorMessage());
+            System.exit(1);
+        }
+        System.out.println("Done!");
+    }
+}
